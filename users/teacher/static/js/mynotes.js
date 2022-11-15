c7ed302011a69dec8e6018f3393193eaf2d1a705
RONIT SINGH <ronitsingh7003@gmail.com>
2022-11-15 15:20:11 +0530

    create note api done

--- users/teacher/static/js/mynotes.js
+++ users/teacher/static/js/mynotes.js
@@ -44,6 +44,7 @@ function getFacultyNoteByIdHTML() {
   document.getElementById("mynote-container").innerHTML = html;
   document.getElementById("show-table").disabled = true;
   document.getElementById("show-form").disabled = false;
+
 }
 
 function showCreateNoteForm() {
@@ -55,26 +56,16 @@ function showCreateNoteForm() {
                             <div class="form-group col-sm-6 my-2">
                                 <label class="text-dark px-2">Select Stream</label>
                                 <div class="col-sm-9">
-                                    <select id="stream" class="form-control">
+                                    <select id="stream" class="form-control" onchange="fetchSemester();">
                                         <option value="" selected disabled>Choose Stream</option>
-                                        <option value="BCA">BCA</option>
-                                        <option value="BBA">BBA</option>
-                                        <option value="MCA">MCA</option>
-                                        <option value="MSC">MSC</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="form-group col-sm-6 my-2">
                                 <label class="text-dark px-2">Select Semester</label>
                                 <div class="col-sm-9">
-                                    <select id="semester" class="form-control">
+                                    <select id="semester" class="form-control" onchange="fetchSubject();">
                                         <option value="" selected disabled>Choose Semester</option>
-                                        <option value="Semester 1">Semester 1</option>
-                                        <option value="Semester 2">Semester 2</option>
-                                        <option value="Semester 3">Semester 3</option>
-                                        <option value="Semester 4">Semester 4</option>
-                                        <option value="Semester 5">Semester 5</option>
-                                        <option value="Semester 6">Semester 6</option>
                                     </select>
                                 </div>
                             </div>
@@ -85,7 +76,6 @@ function showCreateNoteForm() {
                                         <option value="" selected disabled>Choose Section</option>
                                         <option value="Alpha">Alpha</option>
                                         <option value="Beta">Beta</option>
-                                        <option value="Combined">Combined</option>
                                     </select>
                                 </div>
                             </div>
@@ -94,8 +84,6 @@ function showCreateNoteForm() {
                                 <div class="col-sm-9">
                                     <select id="subject" class="form-control">
                                         <option value="" selected disabled>Choose Subject</option>
-                                        <option value="C Language">C Language</option>
-                                        <option value="C++ Language">C++ Language</option>
                                     </select>
                                 </div>
                             </div>
@@ -124,7 +112,7 @@ function showCreateNoteForm() {
                                 </div>
                             </div>
                             <div class="form-group d-flex justify-content-center align-items-center flex-wrap">
-                                <button class="btn btn-success mx-3 mt-2" id="createNoteBtn" type="button">Submit</button>
+                                <button class="btn btn-success mx-3 mt-2" onclick="createNoteAPI();" type="button">Submit</button>
                                 <button class="btn btn-danger mx-3 mt-2" id="cancelCreateNote" onclick="showCreateNoteForm();">Cancel</button>
                             </div>
                             <div style="height:200px; width:100%;">
@@ -135,11 +123,169 @@ function showCreateNoteForm() {
   document.getElementById("mynote-container").innerHTML = html;
   document.getElementById("show-table").disabled = false;
   document.getElementById("show-form").disabled = true;
+
+  document.getElementById("stream").innerHTML = `<option value="" disabled>Loading...!!!</option>`;
+  $.ajax({
+    "url": "/api/v1/data/getAllStream.php",
+    "method": "POST",
+    "timeout": 0,
+  }).done(function (response) {
+    console.log(response);
+    if (response["status_code"] == 1200) {
+      // console.log(response["data"]);
+      let total_record = response["data"].total;
+      let arr = response["data"].streams;
+      let content = `<option value="" selected disabled>Select Stream</option>`;
+      for (let i = 0; i < total_record; i++) {
+        content += `<option value="${arr[i].id}">${arr[i].stream}</option>`;
+      }
+      document.getElementById("stream").innerHTML = content;
+    } else {
+      console.log(["message"]);
+    }
+  });
 }
 
-function deleteNote(id = "") {
+
+async function deleteNote(id) {
   let val = confirm("Are you sure, You want to delete the note?");
-  if (val) {
-    getFacultyNoteByIdHTML();
+  if (!val) {
+    return false;
   }
+
+  var form = new FormData();
+  form.append("id", id);
+
+  var settings = {
+    "url": "/api/v1/scheduleclass/delete.php",
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "contentType": false,
+    "data": form
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    if (response.status_code == 1200) {
+      alert("Class Deleted");
+    } else {
+      alert("An Error Occured");
+      window.location.reload();
+    }
+  });
+
+
 }
+
+
+// Fetching All Semester by Stream Id -------------------------------------------------
+async function fetchSemester(stream_id = 0) {
+  if (stream_id == 0)
+    stream_id = document.getElementById("stream").value;
+  var form = new FormData();
+  form.append("stream_id", stream_id);
+  var settings = {
+    "url": "/api/v1/data/getSemesterByStreamId.php",
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "contentType": false,
+    "data": form
+  };
+
+  document.getElementById("semester").innerHTML = `<option value="" selected disabled>Loading...!!!</option>`;
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    if (response.status_code == "1200") {
+      let total_record = response.data.total;
+      let arr = response.data.semesters;
+      let content = `<option value="" selected disabled>Select Semester</option>`;
+      for (let i = 0; i < total_record; i++) {
+        content += `<option value="${arr[i].id}">${arr[i].sem}</option>`;
+      }
+      document.getElementById("semester").innerHTML = content;
+    } else {
+      console.log(response["message"]);
+    }
+  });
+}
+
+//Fetch all subject by semesterId----------------------------------------------
+
+async function fetchSubject() {
+  let semesters_id = document.getElementById("semester").value;
+  var form = new FormData();
+  form.append("semesters_id", semesters_id);
+  var settings = {
+    "url": "/api/v1/data/getSubjectBySemesterId.php",
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "contentType": false,
+    "data": form
+  };
+
+  document.getElementById("subject").innerHTML = `<option value="" selected disabled>Loading...!!!</option>`;
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    if (response.status_code == "1200") {
+      let total_record = response.data.total;
+      let arr = response.data.subjects;
+      let content = `<option value="" selected disabled>Select Subject</option>`;
+      for (let i = 0; i < total_record; i++) {
+        content += `<option value="${arr[i].id}">${arr[i].subject}</option>`;
+      }
+      document.getElementById("subject").innerHTML = content;
+    } else {
+      console.log(response["message"]);
+    }
+  });
+}
+
+// Create Note API Integration
+
+async function createNoteAPI() {
+  // alert("Here");
+  let stream = document.getElementById("stream").value;
+  let semester = document.getElementById("semester").value;
+  let subject = document.getElementById("subject").value;
+  let topic = document.getElementById("topic").value;
+  let date = document.getElementById("date").value;
+  let recordinglink = document.getElementById("classlink").value;
+  let section = document.getElementById("section").value;
+  let fileInput = document.getElementById("file");
+  var form = new FormData();
+  form.append("faculty_id", login_user.id);
+  form.append("faculty_name", login_user.firstname + " " + login_user.midname + " " + login_user.Lastname);
+  form.append("stream", stream);
+  form.append("sem", semester);
+  form.append("section", section);
+  form.append("subject", subject);
+  form.append("topic", topic);
+  form.append("date", date);
+  form.append("file", fileInput.files[0],);
+  form.append("recordinglink", recordinglink);
+
+  var settings = {
+    "url": "/api/v1/updatednote/create.php",
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "contentType": false,
+    "data": form
+  };
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    if (response.status_code == 1200) {
+      alert(response.message);
+      window.location.reload();
+    } else {
+      alert(response.message);
+      showCreateClassForm();
+    }
+  });
+}
+//
