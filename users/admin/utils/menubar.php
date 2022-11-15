7e10f8525323512a987a873d3285b5a0b9aac1c9
Tania Ghosh <misstaniaghosh2001@gmail.com>
2022-11-15 23:29:18 +0530

    Menubar Changed

--- users/admin/utils/menubar.php
+++ users/admin/utils/menubar.php
@@ -1,3 +1,3 @@
-<li><img src="../static/images/dashboard.png" alt=""><a href="index.php">Dashboard</a></li>
+<li><img src="../static/images/dashboard.png" alt=""><a href="index.php">Home</a></li>
 <li><img src="../static/images/user.png" alt=""><a href="users.php">Users</a></li>
 <li><a href="../../../logout.php"><img src="static/images/log-out.png" alt="">Logout</a></li>
--- users/student/utils/menubar.php
+++ users/student/utils/menubar.php
@@ -1,4 +1,4 @@
-<li><img src="static/images/user.png" alt=""><a href="index.php">Dashboard</a></li>
+<li><img src="static/images/user.png" alt=""><a href="index.php">Home</a></li>
 <!-- <li><img src="static/images/user.png" alt=""><a href="Profile.php">Profile</a></li> -->
 <li><img src="static/images/schedule.png" alt=""><a href="myclass.php">Schedule Class</a></li>
 <li><img src="static/images/edit.png" alt=""><a href="notes.php">Upload Notes</a></li>
